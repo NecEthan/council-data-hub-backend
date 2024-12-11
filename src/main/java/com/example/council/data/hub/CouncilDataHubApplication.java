fc9e2dbef803306e47f666179e0df6e63a53cd61
ethan <ethanowicks9@gmail.com>
2024-12-11 17:41:18 +0000

    remove / in url

--- src/main/java/com/example/council/data/hub/CouncilDataHubApplication.java
+++ src/main/java/com/example/council/data/hub/CouncilDataHubApplication.java
@@ -19,7 +19,7 @@ public class CouncilDataHubApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://council-data-hub.netlify.app/", "http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://council-data-hub.netlify.app", "http://localhost:4200"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
