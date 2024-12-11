48bd096b51ee336370ff50fe572af2859c6d0a5b
ethan <ethanowicks9@gmail.com>
2024-12-11 19:08:45 +0000

    update cors

--- src/main/java/com/example/council/data/hub/CouncilDataHubApplication.java
+++ src/main/java/com/example/council/data/hub/CouncilDataHubApplication.java
@@ -19,16 +19,32 @@ public class CouncilDataHubApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://council-data-hub.netlify.app", "http://localhost:4200"));
-		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
-		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+		corsConfiguration.setAllowedOrigins(Arrays.asList(
+				"https://council-data-hub.netlify.app",
+				"http://localhost:4200"
+		));
+
+		corsConfiguration.setAllowedHeaders(Arrays.asList(
+				"Origin", "Content-Type", "Accept", "Authorization",
+				"X-Requested-With", "Access-Control-Request-Method",
+				"Access-Control-Request-Headers"
+		));
+
+		corsConfiguration.setExposedHeaders(Arrays.asList(
+				"Origin", "Content-Type", "Accept", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"
+		));
+
+		corsConfiguration.setAllowedMethods(Arrays.asList(
+				"GET", "POST", "PUT", "DELETE", "OPTIONS"
+		));
+
+		// Set up URL-based CORS configuration
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+		// Return a new CORS filter based on the configuration
 		return new CorsFilter(urlBasedCorsConfigurationSource);
 	}
-
 }
